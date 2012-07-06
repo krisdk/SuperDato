308c6a360fa69a80c7e57796c20365f81f3682d5
Kristian Nielsen <kristian_n@Kristians-MacBook-9.local>
2012-07-06 14:43:58 +0200

    Initial Commit

---
+++ SuperDato/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SuperDato
+//
+//  Created by Kristian Nielsen on 06/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SuperDatoTests/SuperDatoTests.m
@@ -0,0 +1,32 @@
+//
+//  SuperDatoTests.m
+//  SuperDatoTests
+//
+//  Created by Kristian Nielsen on 06/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "SuperDatoTests.h"
+
+@implementation SuperDatoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SuperDatoTests");
+}
+
+@end
