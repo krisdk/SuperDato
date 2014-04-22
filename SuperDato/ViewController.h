9e40ac8e7c2c4f6ab8efe89211f33e0c2d8c937c
krisdk <kristian@apphaus.dk>
2014-04-22 09:40:26 +0200

    Første upload af Superdato

--- SuperDato/ViewController.h
+++ SuperDato/ViewController.h
@@ -9,5 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *LabelKlokken;
 
+@property (weak, nonatomic) IBOutlet UISwitch *SwitchKlokken;
+- (IBAction)SwitchAktiveret:(id)sender;
 @end
--- SuperDato/ViewController.m
+++ SuperDato/ViewController.m
@@ -3,7 +3,7 @@
 //  SuperDato
 //
 //  Created by Kristian Nielsen on 06/07/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Contentservices.dk. All rights reserved.
 //
 
 #import "ViewController.h"
@@ -13,22 +13,57 @@
 @end
 
 @implementation ViewController
+@synthesize LabelKlokken;
+@synthesize SwitchKlokken;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self OpdaterTid];
+
 }
 
 - (void)viewDidUnload
 {
+    [self setLabelKlokken:nil];
+    [self setSwitchKlokken:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
+}
+
+- (void)OpdaterTid {
+    
+    NSDateFormatter *datoVar = [[NSDateFormatter alloc] init];
+       
+    if(SwitchKlokken.on) {
+        [datoVar setDateFormat: @"dd. MMM yyyy, hh:mm:ss"];
+        LabelKlokken.text =[datoVar stringFromDate:[NSDate date]];
+        
+        
+
+        
+    } else {
+        [datoVar setDateFormat: @"dd. MMM yyyy"];
+        LabelKlokken.text = [datoVar stringFromDate:[NSDate date]];
+       
+        
+
+    }
+
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
+
+- (IBAction)SwitchAktiveret:(id)sender {
+    [self OpdaterTid];
+    
+    
+
+    }
+
 
 @end
